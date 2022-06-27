960ae891947dc2f5fd204c5e637c4001d2f6fab0
jihyeon-kimy <jihyeon.rem@gmail.com>
2022-06-27 23:10:00 +0900

    Add: 3images slider 기능 구현

--- src/components/Main3imagesSlider/Main3imagesSlider.js
+++ src/components/Main3imagesSlider/Main3imagesSlider.js
@@ -1,24 +1,77 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import css from './Main3imagesSlider.module.scss';
 import { MdArrowBackIos, MdArrowForwardIos } from 'react-icons/md';
 
+const infinity = (cardContainerRef, carouselRef, dataLength) => {
+  if (cardContainerRef?.current.scrollLeft === 0) {
+    cardContainerRef.current.style.scrollBehavior = 'auto';
+    cardContainerRef.current.scrollLeft =
+      (carouselRef.current.offsetWidth / 3) * dataLength;
+  }
+
+  if (
+    cardContainerRef?.current.scrollLeft ===
+    (carouselRef.current.offsetWidth / 3) * dataLength * 2
+  ) {
+    cardContainerRef.current.style.scrollBehavior = 'auto';
+    cardContainerRef.current.scrollLeft =
+      (carouselRef.current.offsetWidth / 3) * dataLength;
+  }
+};
+
 function Main3imagesSlider() {
   const [data, setData] = useState([]);
-  const dataLength = data.length - 2;
+  const dataLength = data?.length / 3;
+  let cardContainerRef = useRef();
+  const carouselRef = useRef();
+  let [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
     fetch('http://localhost:3000/data/Main3imagesSlider.json')
       .then(res => res.json())
       .then(fetchdata => {
-        setData([fetchdata[fetchdata.length - 1], ...fetchdata, fetchdata[0]]);
+        setData([...fetchdata, ...fetchdata, ...fetchdata]);
       });
   }, []);
 
+  useEffect(() => {
+    cardContainerRef.current.style.scrollBehavior = 'auto';
+    cardContainerRef.current.scrollLeft =
+      (carouselRef.current.offsetWidth / 3) * dataLength;
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [cardContainerRef.current]);
+
+  const sliderLeft = () => {
+    infinity(cardContainerRef, carouselRef, dataLength);
+
+    cardContainerRef.current.style.scrollBehavior = 'smooth';
+    cardContainerRef.current.scrollLeft -= carouselRef.current.offsetWidth / 3;
+    setCurrentPage(
+      ((cardContainerRef.current.scrollLeft /
+        (carouselRef.current.offsetWidth / 3)) %
+        dataLength) +
+        1
+    );
+  };
+
+  const sliderRight = () => {
+    infinity(cardContainerRef, carouselRef, dataLength);
+
+    cardContainerRef.current.style.scrollBehavior = 'smooth';
+    cardContainerRef.current.scrollLeft += carouselRef.current.offsetWidth / 3;
+    setCurrentPage(
+      ((cardContainerRef.current.scrollLeft /
+        (carouselRef.current.offsetWidth / 3)) %
+        dataLength) +
+        1
+    );
+  };
+
   return (
     <div className={css.component}>
-      <div className={css.carousel}>
+      <div className={css.carousel} ref={carouselRef}>
         <h1 className={css.title}>당신께만 추천드리는 스테이</h1>
-        <div className={css.cardContainer}>
+        <div className={css.cardContainer} ref={cardContainerRef}>
           <div className={css.cards}>
             {data.map((a, i) => {
               return (
@@ -28,7 +81,7 @@ function Main3imagesSlider() {
                     style={{
                       backgroundImage: `url(${data[i].image})`,
                     }}
-                  ></div>
+                  />
                   <div className={css.roomName}>{data[i].room_name}</div>
                   <div className={css.roomInfo}>
                     <span className={css.province}>{data[i].privince}</span>
@@ -43,10 +96,12 @@ function Main3imagesSlider() {
             })}
           </div>
           <div className={css.pagenation}>
-            <span className={`${css.pagNum} ${css.current}`}>3</span>
+            <span className={`${css.pagNum} ${css.current}`}>
+              {currentPage}
+            </span>
             <span className={`${css.pagNum} ${css.total}`}>{dataLength}</span>
-            <MdArrowBackIos className={css.btn} />
-            <MdArrowForwardIos className={css.btn} />
+            <MdArrowBackIos className={css.btn} onClick={sliderLeft} />
+            <MdArrowForwardIos className={css.btn} onClick={sliderRight} />
           </div>
         </div>
       </div>
