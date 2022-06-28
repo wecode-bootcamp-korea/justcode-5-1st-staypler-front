05e312089661d9b814055b3c1420d449424c44f8
jihyeon-kimy <jihyeon.rem@gmail.com>
2022-06-28 14:52:09 +0900

    Add: banner slider 기능구현 - 버튼, 드래그, 마우스 가로휠 조작을 통한 무한루프 슬라이더

--- src/components/ReservationSlider/ReservationSlider.js
+++ src/components/ReservationSlider/ReservationSlider.js
@@ -1,11 +1,29 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import css from './ReservationSlider.module.scss';
 import { MdArrowBackIos, MdArrowForwardIos } from 'react-icons/md';
+
+const infinity = (cardContainerRef, carouselRef, dataLength) => {
+  if (cardContainerRef.current.scrollLeft === 0) {
+    cardContainerRef.current.style.scrollBehavior = 'auto';
+    cardContainerRef.current.scrollLeft =
+      carouselRef.current.offsetWidth * dataLength;
+  }
+
+  if (
+    cardContainerRef.current.scrollLeft ===
+    carouselRef.current.offsetWidth * (dataLength + 1)
+  ) {
+    cardContainerRef.current.style.scrollBehavior = 'auto';
+    cardContainerRef.current.scrollLeft = carouselRef.current.offsetWidth;
+  }
+};
 
 function ReservationSlider() {
   const [data, setData] = useState({});
   const dataLength = data.images?.length;
   let dataImages = [];
+  let cardContainerRef = useRef();
+  const carouselRef = useRef();
 
   useEffect(() => {
     fetch('http://localhost:3000/data/ReservationSlider.json')
@@ -18,6 +36,69 @@ function ReservationSlider() {
   if (data.images !== undefined) {
     dataImages = [data.images[dataLength - 1], ...data.images, data.images[0]];
   }
+
+  useEffect(() => {
+    cardContainerRef.current.style.scrollBehavior = 'auto';
+    cardContainerRef.current.scrollLeft = carouselRef.current.offsetWidth;
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [cardContainerRef.current]);
+
+  const sliderLeft = () => {
+    infinity(cardContainerRef, carouselRef, dataLength);
+    cardContainerRef.current.style.scrollBehavior = 'smooth';
+    cardContainerRef.current.scrollLeft -= carouselRef.current.offsetWidth;
+  };
+
+  const sliderRight = () => {
+    infinity(cardContainerRef, carouselRef, dataLength);
+    cardContainerRef.current.style.scrollBehavior = 'smooth';
+    cardContainerRef.current.scrollLeft += carouselRef.current.offsetWidth;
+  };
+
+  useEffect(() => {
+    let isPressedDown = false;
+    let cursorXspace;
+
+    const carouselRefCurrent = carouselRef.current;
+
+    function mouseUpCallback() {
+      isPressedDown = false;
+    }
+
+    function mouseDownCallback(e) {
+      isPressedDown = true;
+      cursorXspace = e.offsetX - cardContainerRef.current.offsetLeft;
+    }
+
+    function mouseMoveCallback(e) {
+      infinity(cardContainerRef, carouselRef, dataLength);
+
+      if (!isPressedDown) return;
+      e.preventDefault();
+      cardContainerRef.current.style.scrollBehavior = 'smooth';
+      if (cursorXspace - e.offsetX > carouselRefCurrent.offsetWidth * 0.2) {
+        cardContainerRef.current.scrollLeft += carouselRefCurrent.offsetWidth;
+      } else if (
+        cursorXspace - e.offsetX <
+        carouselRefCurrent.offsetWidth * -0.2
+      ) {
+        cardContainerRef.current.scrollLeft -= carouselRefCurrent.offsetWidth;
+      }
+    }
+
+    window.addEventListener('mouseup', mouseUpCallback);
+    carouselRefCurrent.addEventListener('mousedown', mouseDownCallback);
+    carouselRefCurrent.addEventListener('mouseup', mouseUpCallback);
+    carouselRefCurrent.addEventListener('mousemove', mouseMoveCallback);
+
+    return () => {
+      window.removeEventListener('mouseup', mouseUpCallback);
+      carouselRefCurrent.removeEventListener('mousedown', mouseDownCallback);
+      carouselRefCurrent.removeEventListener('mouseup', mouseUpCallback);
+      carouselRefCurrent.removeEventListener('mousemove', mouseMoveCallback);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [carouselRef.current]);
 
   return (
     <div className={css.component}>
@@ -38,16 +119,16 @@ function ReservationSlider() {
             </div>
           </div>
         </div>
-        <div className={css.carousel}>
-          <div className={css.cardContainer}>
-            {data.images &&
-              data.images.map((a, i) => {
+        <div className={css.carousel} ref={carouselRef}>
+          <div className={css.cardContainer} ref={cardContainerRef}>
+            {dataImages &&
+              dataImages.map((a, i) => {
                 return (
                   <div className={css.card} key={i}>
                     <div
                       className={css.roomImg}
                       style={{
-                        backgroundImage: `url(${data.images[i]})`,
+                        backgroundImage: `url(${dataImages[i]})`,
                       }}
                       key={i}
                     />
@@ -56,10 +137,10 @@ function ReservationSlider() {
               })}
           </div>
           <div className={css.sliderBtn}>
-            <div>
+            <div onClick={sliderLeft}>
               <MdArrowBackIos size={20} />
             </div>
-            <div>
+            <div onClick={sliderRight}>
               <MdArrowForwardIos size={20} />
             </div>
           </div>
