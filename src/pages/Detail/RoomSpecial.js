9a5aa2885993d8a3bb0bddf6eb75912db2c258bd
Shinhye Kim <cindy.hye2@gmail.com>
2022-06-28 16:52:04 +0900

    Add: roomSpecial

---
+++ src/pages/Detail/RoomSpecial.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import style from './RoomSpecial.module.scss';
+
+const RoomSpecial = ({ details }) => {
+  const special = details.length > 0 ? details[0].special : null;
+  console.log('special:', special);
+
+  return (
+    <div className={style.container}>
+      <div className={style.specialWrap}>
+        <div className={style.special}>SPECIAL</div>
+        <div className={style.speailListWrap}>
+          {special &&
+            special.map((el, idx) => {
+              return (
+                <div className={style.contentWrap} key={el.id}>
+                  <img src={el.image} alt={el.id} className={style.icon} />
+                  <div className={style.title}>{el.title}</div>
+                  <div className={style.content}>{el.content}</div>
+                </div>
+              );
+            })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RoomSpecial;
