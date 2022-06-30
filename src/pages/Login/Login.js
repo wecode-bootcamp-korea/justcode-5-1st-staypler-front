ba6141b4ed11c1289a9e0bd51d98800f570000be
mulnon2g <carryy333@gmail.com>
2022-06-30 19:43:33 +0900

    Modify: Login Token Setup

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import css from './Login.module.scss';
 
 function Login() {
+  const navigate = useNavigate();
   const [inputValue, setInputValue] = useState({
     email: '',
     password: '',
@@ -14,60 +15,35 @@ function Login() {
       [name]: value,
     });
   };
-  const navigate = useNavigate();
-  const goToHome = () => {
-    // fetch('http://192.168.1.4:10010/users/login', {
-    //   method: 'POST',
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //   },
-    //   body: JSON.stringify({
-    //     email: inputValue.email,
-    //     password: inputValue.password,
-    //   }),
-    // })
-    //   .then(response => response.json())
-    //   .then(result =>
-    //     result.token !== undefined
-    //       ? navigate('/home')
-    //       : alert('아이디혹은 비밀번호가 잘못되었습니다')
-    //   );
+  const loginBtnHandle = event => {
+    event.preventDefault();
+    fetch('http://192.168.1.4:10010/users/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email: inputValue.email,
+        password: inputValue.password,
+      }),
+    })
+      .then(response => response.json())
+      .then(result => {
+        if (result.token) {
+          // console.log(result);
+          localStorage.setItem('login-token', result.token);
+          navigate('/');
+        } else {
+          alert('아이디혹은 비밀번호가 잘못되었습니다');
+        }
+      });
   };
-  const regEmail =
-    /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
-  const regSpecialCharacter =
-    /[\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\']/g;
-  const regNumber = /[0-9]/g;
-  const regString = /[a-zA-Z]/g;
-  const [emailValid, setEmailValid] = useState(false);
-  const [passwordValid, setPasswordValid] = useState(false);
-  const emailValidation = () => {
-    if (regEmail.test(inputValue.email)) {
-      setEmailValid(true);
-    } else {
-      setEmailValid(false);
-    }
+
+  const signUpBtnHandle = event => {
+    event.preventDefault();
+    navigate('/signup');
   };
-  const passwordValidation = () => {
-    if (
-      8 < inputValue.password.length &&
-      inputValue.password.length < 20 &&
-      regNumber.test(inputValue.password) &&
-      regString.test(inputValue.password) &&
-      regSpecialCharacter.test(inputValue.password)
-    ) {
-      setPasswordValid(true);
-    } else {
-      setPasswordValid(false);
-    }
-  };
-  const loginBtnActivation = () => {
-    if (emailValid && passwordValid === true) {
-      goToHome();
-    } else {
-      alert('아이디 혹은 비밀번호가 잘못되었습니다');
-    }
-  };
+
   return (
     <div className={css.container}>
       <header>
@@ -84,31 +60,29 @@ function Login() {
                 type="text"
                 placeholder="이메일 아이디"
                 onChange={handleInput}
-                onKeyUp={emailValidation}
               />
             </div>
             <div className={css.inputbox}>
               <div className={css.title}>비밀번호</div>
               <input
                 type="password"
-                name="name"
+                name="password"
                 placeholder="비밀번호"
                 onChange={handleInput}
-                onKeyUp={passwordValidation}
               />
             </div>
           </div>
           <button
             type="button"
             className={css.loginBtn}
-            onClick={loginBtnActivation}
+            onClick={loginBtnHandle}
           >
             로그인
           </button>
           <button
             type="button"
             className={css.signupBtn}
-            onClick={navigate('/users/signup')}
+            onClick={signUpBtnHandle}
           >
             회원가입
           </button>
