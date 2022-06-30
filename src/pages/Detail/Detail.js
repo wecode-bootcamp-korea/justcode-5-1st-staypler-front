827931683ca225fa7a2d4b7668fb0b138e0e6b05
Shinhye Kim <cindy.hye2@gmail.com>
2022-07-01 06:53:27 +0900

    Fix: detailpage 임시commit

--- src/pages/Detail/Detail.js
+++ src/pages/Detail/Detail.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import DetailBannerSlider from '../../components/DetailBannerSlider/DetailBannerSlider';
 import Like from '../../components/Like/Like';
 import RoomInfoSlider from './RoomInfoSlider';
@@ -10,22 +10,32 @@ import FAQ from './FAQ/FAQ';
 function Detail() {
   const [data, setData] = useState([]);
   let { id } = useParams();
+  console.log(id);
   useEffect(() => {
-    fetch(`http://192.168.1.4:10010/rooms/${id}`)
+    fetch(`http://localhost:10010/rooms/${id}`, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('login-token')}`,
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    })
       .then(res => {
         if (res.ok) {
           return res.json();
         }
       })
       .then(fetchdata => {
+        console.log('data:', fetchdata);
         setData(fetchdata.data[0]);
       });
   }, []);
+  // console.log(data);
   return (
     <div className={css.container}>
       <div className={css.roomName}>
         {data.room_name}
-        {data && <Like data={data} />}
+        {data && <Like id={id} isLike={data.islike} />}
       </div>
       {data && <DetailBannerSlider roomData={data} />}
       {data && <RoomInfoSlider roomData={data} />}
