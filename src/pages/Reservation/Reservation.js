d0df07064ed072e371e73e525dbee9b15b258fe8
mulnon2g <carryy333@gmail.com>
2022-07-01 12:31:01 +0900

    ADD:reservation page

--- src/pages/Reservation/Reservation.js
+++ src/pages/Reservation/Reservation.js
@@ -1,14 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import ReservationSlider from '../../components/ReservationSlider/ReservationSlider';
+import ReservationHeader from './ReservationHeader';
 
 import css from './Reservation.module.scss';
 
 function Reservation() {
   const { roomid } = useParams();
+  const [data, setData] = useState({});
+  useEffect(() => {
+    fetch(`http://192.168.1.6:10010/rooms/${roomid}/room`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    })
+      .then(res => {
+        if (res.ok) {
+          return res.json();
+        }
+      })
+      .then(fetchdata => {
+        setData(fetchdata.data[0]);
+      });
+  }, []);
+
+  // console.log(data);
   return (
     <div className={css.container}>
-      <ReservationSlider roomid={roomid} />
+      <ReservationHeader roomid={roomid} roomname={data?.room_name} />
+      {data && (
+        <ReservationSlider
+          roomid={roomid}
+          data={data}
+          imageLength={data.images?.length}
+        />
+      )}
     </div>
   );
 }
---
+++ src/pages/Reservation/ReservationHeader.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import css from './ReservationHeader.module.scss';
+function ReservationHeader(props) {
+  const [data, setData] = useState({});
+
+  return (
+    <div className={css.container}>
+      <div className={css.header}>
+        <div className={css.title}>BOOKING</div>
+        <div className={css.bookingRow}>
+          <div className={css.name}>{props.roomname}</div>
+          <div className={css.bookingDate}>
+            <input
+              type="text"
+              className={css.date}
+              placeholder="여행시작 yyyy-mm-dd"
+            />
+            <input
+              type="text"
+              className={css.date}
+              placeholder="여행마감 yyyy-mm-dd"
+            />
+          </div>
+          <button className={css.bookingBtn}>결제하기</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default ReservationHeader;
